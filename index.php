cbdb6cdb7bcaf75f77d21509ed870a457995ab91
Andrey Pechersky <andrey.pechesky@gmail.com>
2015-01-09 18:21:12 +0200

    asdf

--- index.php
+++ index.php
@@ -1,6 +1,31 @@
 <?php
 
+ /* data = file_get_contents('http://wp-admin.com.ua/nahodim-vse-kartinki-v-tekste/');
+$images = array();
+preg_match_all('/(img|src)=("|\')[^"\'>]+/i', $data, $media);
+unset($data);
+$data = preg_replace('/(img|src)("|\'|="|=\')(.*)/i', "$3", $media[0]);
+ 
+foreach ($data as $url) {
+    $info = pathinfo($url);
+    if (isset($info['extension'])) {
+        if (($info['extension'] == 'jpg') ||
+                ($info['extension'] == 'jpeg') ||
+                ($info['extension'] == 'gif') ||
+                ($info['extension'] == 'png'))
+            array_push($images, $url);
+    }
+}
 
+foreach ($images as $key => $value) {
+	$img = $value;
+	if ( strpos($img, 'http://') === false) $img = 'http://wp-admin.com.ua' . $img;
+	echo "<img src='" . $img . "'></img> <br />";
+
+}
+
+
+die(); */
 
 require_once('webp.php');
 
